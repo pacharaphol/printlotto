eae9cede66f1788a7ba317d0bef03a7d72350e41
pacharaphol <ko.pacharaphol_st@tni.ac.th>
2016-12-16 03:03:13 +0000

    First commit

--- hello-cpp-world.cc
+++ hello-cpp-world.cc
@@ -1,5 +1,17 @@
-#include <iostream>
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+ 
+ 
+int main(){
+	srand(time(NULL));
+ 
+	int randomNumber1 = rand() % 10; //0-10
+	int randomNumber2 = rand() % 10; //0-10
+	int randomNumber3 = 50+(rand()%(10-50)); //50-200
+ 
+	cout<<("Random number1 is %d\n", randomNumber1);
+	cout<<("Random number2 is %d\n", randomNumber2);
+	cout<<("Random number3 is %d\n", randomNumber3);
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
 }
